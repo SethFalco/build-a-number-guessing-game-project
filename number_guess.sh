b2c3a58521378e84da4c64fd7bf10d85bc072e69
SethFalco <seth@falco.fun>
2023-05-16 18:52:43 +0000

    feat: add guessing loop

--- number_guess.sh
+++ number_guess.sh
@@ -17,3 +17,26 @@ fi
 
 GUESSES=0
 SECRET=$(( RANDOM % 1000 + 1 ))
+
+echo "Guess the secret number between 1 and 1000:"
+
+while [[ $GUESS != $SECRET ]]
+do
+  read GUESS
+  
+  if [[ ! $GUESS =~ [0-9]+ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  GUESSES=$(( GUESSES + 1 ))
+
+  if [[ $GUESS -gt $SECRET ]]
+  then
+    echo "It's lower than that, guess again:"
+  elif [[ $GUESS -lt $SECRET ]]
+  then
+    echo "It's higher than that, guess again:"
+  fi
+done
